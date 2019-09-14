0d9b8ffa32d82e39b4db7116d7b23c269f321e71
Justin Cook <justin.cook.dev@gmail.com>
2019-09-14 10:59:49 -0700

    Find loop working!

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,60 @@
 'use strict';
 
-//Complete this algo
-const isLoop = (linkedlist) => {
+const checkForwardLoop = (linkedlist, returning = false) => {
+  const visitedNodes = new Map();
 
+  let node = linkedlist.head;
+
+  while (node) {
+    visitedNodes.set(node, true);
+
+    if (visitedNodes.has(node.next)) {
+      if (returning) {
+        return node;
+      } else {
+        return true;
+      }
+    } else {
+      node = node.next;
+    }
+  }
+
+  return false;
 };
 
+const checkReverseLoop = (linkedlist, returning = false) => {
+  const visitedNodes = new Map();
+
+  let node = linkedlist.tail;
+
+  while (node) {
+    visitedNodes.set(node, true);
+
+    if (visitedNodes.has(node.previous)) {
+      if (returning) {
+        return node;
+      } else {
+        return true;
+      }
+    } else {
+      node = node.previous;
+    }
+  }
+
+  return false;
+};
+
+//Complete this algo
+const isLoop = linkedlist => {
+  return checkForwardLoop(linkedlist) || checkReverseLoop(linkedlist);
+};
+
+const findLoop = linkedlist => {
+  const loopStart =
+    checkReverseLoop(linkedlist, true) || checkForwardLoop(linkedlist, true);
+
+  return loopStart ? loopStart.value : false;
+};
 
 /*
 EXTRA CREDIT:
@@ -14,4 +64,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = { isLoop, findLoop };
