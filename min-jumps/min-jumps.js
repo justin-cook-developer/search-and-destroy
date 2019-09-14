899831c8af0080f689fc01a78f0e33d686ec83cd
Justin Cook <justin.cook.dev@gmail.com>
2019-09-14 12:01:25 -0700

    passing!!!

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,72 @@
 'use strict';
 
-// Complete this algo
-const minJumps = arr => {
+// [2, 1, 2, 3, 4]
 
+// 2 => 1
+// [1, 2, 3, 4]
+
+// 1 => 2
+// [2, 3, 4]
+
+// 2 => 4
+// [4]
+
+// 2 => 3
+// [3, 4]
+
+// 3 => 4
+// [4]
+
+// 2 => 2
+// [2, 3, 4]
+
+// 2 => 4
+// [4]
+
+// 2 => 3
+// [3, 4]
+
+// 3 => 4
+// 4
+
+// [2, 1, 2, 0, 4]
+
+// [[1, 2, 0, 4], [2, 0, 4]]
+
+// 1 + func([1, 2, 0, 4]), 1 + func([2, 0, 4])
+// 1 + func([2, 0, 4])
+// 1 + func([0, 4]), 1 + func([4])
+// problem, 1
+
+const generatePossibilities = arr => {
+  const possibilities = [];
+  const possibleJumps = arr[0];
+
+  for (let i = 1; i <= possibleJumps; i++) {
+    const possible = arr.slice(0 + i);
+    possibilities.push(possible);
+  }
+
+  return possibilities;
 };
 
-module.exports = minJumps
+// Complete this algo
+const minJumps = arr => {
+  if (arr.length <= 1) {
+    return 0;
+  } else {
+    const possibilities = generatePossibilities(arr);
+
+    const numJumps = possibilities.map(p => {
+      if (p[0] === 0) {
+        return Infinity;
+      } else {
+        return 1 + minJumps(p);
+      }
+    });
+
+    return Math.min(...numJumps);
+  }
+};
+
+module.exports = minJumps;
